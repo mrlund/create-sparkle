d32eb3802b207d5155dd38e6aa3ec23aafb10d71
Akash <borad.akash@gmail.com>
2020-11-10 16:55:10 +0530

    starter: update to ionic pwa stencil starter

---
+++ src/global/app.ts
@@ -0,0 +1,12 @@
+import '@ionic/core';
+
+import "@sparkle-learning/core";
+
+
+// import { setupConfig } from '@ionic/core';
+
+export default () => {
+  // setupConfig({
+  //   mode: 'ios'
+  // });
+};
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import "../node_modules/@sparkle-learning/core/dist/sparkle-core/sparkle-core.css";
-
-import { defineCustomElements as sparkleElements } from "@sparkle-learning/core/dist/loader";
-
-sparkleElements();
